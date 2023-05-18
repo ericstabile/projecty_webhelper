385f562140718a980f6969b0edde92ba0856c479
ericstabile <esstabile@gmail.com>
2023-05-17 20:22:58 -0500

    updated redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 import './Assets/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavigation from './Navigation/AppNavigation';
@@ -7,10 +7,18 @@ import HomePage from './HomePage/HomePage';
 import InventoryServiceMainPage from './Inventory/InventoryServiceMainPage/InventoryServiceMainPage';
 import AddNewInventoryObjectPage from './Inventory/AddNewInventoryObjectPage/AddNewInventoryObjectPage';
 
+const RedirectPage = () => {
+  useEffect(() => {
+    window.location.href = '/';
+  }, []);
+
+  return null;
+};
+
 const App = () => {
   const [inventoryObjectData, setInventoryObjectData] = useState([]);
   const [lastID, setLastID] = useState(0);
-  
+
   const handleAddItem = (item) => {
     const newItem = { ...item, ID: lastID + 1 };
     setInventoryObjectData([...inventoryObjectData, newItem]);
@@ -26,30 +34,30 @@ const App = () => {
       <div className="App">
         <AppNavigation />
         <Routes>
-          <Route 
-            path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route
             path="/inventory"
-            element=
-            {
-              <InventoryServiceMainPage 
-                inventoryObjectData={inventoryObjectData || []}  
-                setInventoryObjectData={setInventoryObjectData} 
-                setLastID={setLastID} />
+            element={
+              <InventoryServiceMainPage
+                inventoryObjectData={inventoryObjectData || []}
+                setInventoryObjectData={setInventoryObjectData}
+                setLastID={setLastID}
+              />
             }
           />
           <Route
             path="/add"
-            element=
-            {
-              <AddNewInventoryObjectPage 
-                onAddItem={handleAddItem} 
-                inventoryObjectData={inventoryObjectData || []}  
-                setInventoryObjectData={setInventoryObjectData} 
-                lastID={lastID + 1} 
-                handleFileChosen={handleFileChosen} />
+            element={
+              <AddNewInventoryObjectPage
+                onAddItem={handleAddItem}
+                inventoryObjectData={inventoryObjectData || []}
+                setInventoryObjectData={setInventoryObjectData}
+                lastID={lastID + 1}
+                handleFileChosen={handleFileChosen}
+              />
             }
           />
+          <Route path="/projecty_webhelper/*" element={<RedirectPage />} />
         </Routes>
       </div>
     </Router>
